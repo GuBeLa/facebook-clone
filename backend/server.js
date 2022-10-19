f0ed6cf5b6744455d08c083f305ee902f56e1130
Irakli Gubelidze <igubelidze@ip.local>
2022-10-19 11:50:07 +0400

    Register and validate data and using Regex to validate email

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,11 @@ app.use(cors());
 readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)))
 
 // database
-mongoose.connect(process.env.DATABASE_URL, {
-  useNewUrlParser: true
-})
-.then(() => console.log("database connected successfylly"))
-.catch((err) => console.log(`error connecting to mongodb ${err}`))
+// mongoose.connect(process.env.DATABASE_URL, {
+//   useNewUrlParser: true
+// })
+// .then(() => console.log("database connected successfylly"))
+// .catch((err) => console.log(`error connecting to mongodb ${err}`))
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
