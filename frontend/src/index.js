f4ec9d75d3faf466fb6ce462d62342eaf36b831d
Irakli Gubelidze <igubelidze@ip.local>
2022-10-27 11:50:10 +0400

    login and registration form formik

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import "./styles/icons/icons.css";
 import App from "./App";
-import { legacy_createStore as createStore } from "redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import { createStore } from "redux";
 import { Provider } from "react-redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./reducers";
